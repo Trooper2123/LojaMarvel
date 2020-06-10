4e90756fc9bf58970d2406a25fa658c000d4d76b
TLBN <tete.leticia@gmail.com>
2020-06-10 12:28:50 -0300

    Initial commit

---
+++ src/com/company/Client.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Date;
+
+public class Client {
+    public String nome;
+    private Date bday;
+    private String cpf;
+    public String email;
+    public String phone;
+    public String cep;
+
+}
